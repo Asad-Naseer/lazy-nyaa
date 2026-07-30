cbe635a7d6452b357f01fcb8ef75ef7d78fd01ef
Asad-Naseer <asadn66r6@gmail.com>
2026-07-31 02:17:27 +0500

    fix: minor typo

--- lazy-nyaa.py
+++ lazy-nyaa.py
@@ -41,7 +41,7 @@ class lazy_nyaa(App):
     def copy_magnet(self, event:ListView.Selected):
         if sys.platform == "darwin":
             process = subprocess.Popen("pbcopy", stdin=subprocess.PIPE, text=True) # roughly equivalent to doing this: echo "magnet_link" | pbcopy
-            proces.communicate(event.item.magnet_link) # this is what we are sending to the mac clipboard
+            process.communicate(event.item.magnet_link) # this is what we are sending to the mac clipboard
         else:
             self.app.copy_to_clipboard(event.item.magnet_link)
         self.notify("Magnet Link Copied!")
